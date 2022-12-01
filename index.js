d739ca4240c6cc04b222df8e8bd7694ede65c176
azizulhakims <it.azizulhakim@gmsil.com>
2022-12-01 11:00:26 +0600

    add all booked items

--- index.js
+++ index.js
@@ -22,10 +22,21 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const oldLaptopCollection = client.db('oldlaptop').collection('oldlaptopuser')
+        const oldLaptopCollectionCategory = client.db('oldlaptop').collection('oldLaptopCategory')
+        const oldLaptopCollectionItems = client.db('oldlaptop').collection('oldLaptopItems')
+        const oldLaptopAddProductCollection = client.db('oldlaptop').collection('addproduct');
+        const oldLaptopProductBookCollection = client.db('oldlaptop').collection('productbook');
+
+        //http://localhost:5000/productbook
 
         console.log("database connected..");
 
         //JWT
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const result = await oldLaptopCollection.findOne({ email });
+            res.send(result);
+        })
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email
             const user = req.body
@@ -45,6 +56,97 @@ async function run() {
             res.send({ result, token })
 
         })
+
+        app.get('/oldLaptopCategory', async (req, res) => {
+            const query = {};
+            const category = await oldLaptopCollectionCategory.find(query).toArray();
+            res.send(category);
+        })
+
+        // app.get('/categoryTitle', async (req, res) => {
+        //     const query = {}
+        //     const result = await oldLaptopCollectionCategory.find(query).project({ name: 1 }).toArray();
+        //     res.send(result);
+        // })
+
+        app.get('/addproduct', async (req, res) => {
+            const query = {};
+            const product = await oldLaptopAddProductCollection.find(query).toArray();
+            res.send(product);
+
+        })
+
+
+        app.post('/addproduct', async (req, res) => {
+            const doctor = req.body;
+            const result = await oldLaptopAddProductCollection.insertOne(doctor);
+            res.send(result);
+        })
+
+        app.delete('/addproduct/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await oldLaptopAddProductCollection.deleteOne(filter);
+            res.send(result);
+
+        })
+
+        // app.get('/oldLaptopCategory/:id', (req, res) => {
+        //     const id = req.params.id;
+        //     const selectedCategory = oldLaptopCollectionCategory.find(c => c._id === id);
+        //     res.send(selectedCategory);
+        //     // console.log(req.params.id);
+        // })
+
+
+
+        app.post('/productbook', async (req, res) => {
+            const productbook = req.body;
+            const result = await oldLaptopProductBookCollection.insertOne(productbook);
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+        //for user
+        // app.get('/productbook', async (req, res) => {
+        //     const { email } = req.query;
+
+        //     const productbook = await oldLaptopProductBookCollection.find({ email }).toArray();
+        //     res.send(productbook);
+
+        // })
+
+        //for admin
+
+        app.get('/productbook', async (req, res) => {
+            let query = {}
+            const email = req.query.email
+
+            if (email) {
+                query = {
+                    email: email,
+                }
+            }
+            const productbook = await oldLaptopProductBookCollection.find(query).toArray()
+            console.log(productbook);
+
+            res.send(productbook);
+        })
+
+
+        // app.get('/addproduct', async (req, res) => {
+        //     const query = {};
+        //     const product = await oldLaptopAddProductCollection.find(query).toArray();
+        //     res.send(product);
+
+
+
 
     }
     finally {
